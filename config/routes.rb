c61b9e6300c2c0f6e765644d32ce60e0dd4dda13
kura-masahiro <dacanghongsheng@gmail.com>
2020-09-03 15:27:40 +0900

    electric_postsコントローラとビューファイルを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'electric_posts/index'
+  get 'electric_posts/show'
+  get 'electric_posts/new'
+  get 'electric_posts/create'
+  get 'electric_posts/edit'
+  get 'electric_posts/update'
+  get 'electric_posts/destroy'
   devise_for :users
   root 'pages#index'
   resources :posts
