585f68f79a01202bec96e42bb412e272d6848ac6
kura-masahiro <dacanghongsheng@gmail.com>
2020-09-05 14:53:42 +0900

    ルーティングを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,11 @@
 Rails.application.routes.draw do
- 
-  get 'boil_posts/index'
-  get 'boil_posts/show'
-  get 'boil_posts/new'
-  get 'boil_posts/create'
-  get 'boil_posts/edit'
-  get 'boil_posts/update'
-  get 'boil_posts/destroy'
+
   devise_for :users
   root 'pages#index'
   resources :posts
   resources :electric_posts
+  resources :boil_posts
+  
   get 'pages/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
