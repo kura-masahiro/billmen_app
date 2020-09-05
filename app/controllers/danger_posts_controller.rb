3b4d759d5dd4f581d7ca98af1a52f928dc1e3053
kura-masahiro <dacanghongsheng@gmail.com>
2020-09-05 21:33:50 +0900

    danger_postsのコントローラーとビューを各種修正

--- app/controllers/danger_posts_controller.rb
+++ app/controllers/danger_posts_controller.rb
@@ -1,22 +1,55 @@
 class DangerPostsController < ApplicationController
+   before_action :set_danger_post, only: %i[show edit update destroy]
+   before_action :authenticate_user!, except: :index
+
   def index
+    @danger_posts = DangerPost.order(id: :asc)
   end
 
   def show
+
   end
 
   def new
+    @danger_post = DangerPost.new
   end
 
   def create
+    @danger_post = DangerPost.new(danger_post_params)
+    if @danger_post.save
+      redirect_to @danger_post, notice: "投稿しました"
+    else
+      flash.now[:alert] = "投稿に失敗しました"
+      render :new
+    end
   end
 
   def edit
+
   end
 
   def update
+    if @danger_post.update(danger_post_params)
+      redirect_to @danger_post, notice: "更新しました"
+    else
+      flash.now[:alert] = "更新に失敗しました"
+      render :edit
+    end
   end
 
   def destroy
+    @danger_post.destroy!
+    redirect_to @danger_post, alert: "削除しました"
   end
+
+  private
+
+  def set_danger_post
+    @danger_post = DangerPost.find(params[:id])
+  end
+
+  def danger_post_params
+    params.require(:danger_post).permit(:title, :content).merge(user_id: current_user.id)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,12 @@
 Rails.application.routes.draw do
-  get 'danger_posts/index'
-  get 'danger_posts/show'
-  get 'danger_posts/new'
-  get 'danger_posts/create'
-  get 'danger_posts/edit'
-  get 'danger_posts/update'
-  get 'danger_posts/destroy'
+
   devise_for :users
   root 'pages#index'
   resources :posts
   resources :electric_posts
   resources :boil_posts
   resources :freeze_posts
+  resources :danger_posts
   get 'pages/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
