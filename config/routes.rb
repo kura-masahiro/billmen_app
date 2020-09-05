adc489365240fd1c54199c710195bb6968530778
kura-masahiro <dacanghongsheng@gmail.com>
2020-09-05 18:06:55 +0900

    ルーティングを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,10 @@
 Rails.application.routes.draw do
-  get 'freeze_posts/index'
-  get 'freeze_posts/show'
-  get 'freeze_posts/new'
-  get 'freeze_posts/create'
-  get 'freeze_posts/edit'
-  get 'freeze_posts/update'
-  get 'freeze_posts/destroy'
   devise_for :users
   root 'pages#index'
   resources :posts
   resources :electric_posts
   resources :boil_posts
+  resources :freeze_posts
   get 'pages/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
