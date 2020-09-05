736bd23f01d63c9729bc7ce77c033419a0672a05
kura-masahiro <dacanghongsheng@gmail.com>
2020-09-05 14:48:31 +0900

    BoilPostモデルとboil_postsテーブルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_04_070414) do
+ActiveRecord::Schema.define(version: 2020_09_05_054539) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "boil_posts", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "electric_posts", force: :cascade do |t|
     t.string "title"
---
+++ test/models/boil_post_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class BoilPostTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
